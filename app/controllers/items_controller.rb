18ca2ca3293ec2694eaf44df370964cd5dfdbfde
George Samuel <geosam2708@gmail.com>
2023-06-27 00:01:31 +0300

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+
   def index
     if params[:user_id]
       return render json: User.find(params[:user_id]).items
@@ -16,13 +18,17 @@ class ItemsController < ApplicationController
   def create
     user = User.find(params[:user_id])
     user.items.create(item_params)
-    render json: user.items.last, except: [:created_at, :updated_at]
+    render json: user.items.last, except: [:created_at, :updated_at], status: :created
   end
 
   private
 
   def item_params
     params.permit(:name, :description, :price)
+  end
+
+  def record_not_found_response
+    render json: {error: "Couldn't find record with #{params[:id]}"}, status: :not_found
   end
 
 end
